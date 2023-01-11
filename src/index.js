e9d98d099e45f2ed87ddd59b15c1bc5d8ae9ace9
Joona Kemppainen <joona.kemppainen@pm.me>
2023-01-11 21:22:37 +0100

    disable react dev tools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import './index.css';
 import App from './App';
+
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
